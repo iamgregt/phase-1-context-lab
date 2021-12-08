91d301d2801b03f0b4f7902f6385446e46b0b814
Gregory <taylor.gregory2@gmail.com>
2021-12-07 19:57:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-/* Your Code Here */
+function createEmployeeRecord(array){
+    let obj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return obj
+}
 
+function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map(array => {
+        createEmployeeRecord(array)
+    })
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
